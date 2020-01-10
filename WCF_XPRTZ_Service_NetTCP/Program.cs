7bdcf0d24311a045b430440dc7e97823b59b79df
stefan van tilborg <stefan.tilborg@politie.nl>
2020-01-10 10:07:26 +0100

    Add project files.

---
+++ WCF_XPRTZ_Service_NetTCP/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WCF_XPRTZ_Service_NetTCP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
